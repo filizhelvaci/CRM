2b94b28f016e8d4fddfaf223e73fb67a0a767b1f
Filiz Helvaci <filizhelvaci82@gmail.com>
2021-04-29 13:43:05 +0300

    crm_cltvp.py eklendi

---
+++ crm_cltvp.py
@@ -0,0 +1,234 @@
+import datetime as dt
+import pymysql
+import pandas as pd
+from lifetimes import BetaGeoFitter
+from lifetimes import GammaGammaFitter
+from sklearn.preprocessing import MinMaxScaler
+from sqlalchemy import create_engine
+
+pd.set_option('display.max_columns', None)
+pd.set_option('display.max_columns', None)
+
+df_ = pd.read_excel("dataset/online_retail_II.xlsx",sheet_name="Year 2010-2011")
+df = df_.copy()
+
+
+def check_df(dataframe):
+    print("##################### Shape #####################")
+    print(dataframe.shape)
+    print("##################### Types #####################")
+    print(dataframe.dtypes)
+    print("##################### Head #####################")
+    print(dataframe.head(3))
+    print("##################### NA #####################")
+    print(dataframe.isnull().sum())
+    print("##################### Quantiles #####################")
+    print(dataframe.quantile([0, 0.05, 0.50, 0.95, 0.99, 1]).T)
+
+
+def outlier_thresholds(dataframe, variable):
+    quartile1 = dataframe[variable].quantile(0.01)
+    quartile3 = dataframe[variable].quantile(0.99)
+    interquantile_range = quartile3 - quartile1
+    up_limit = quartile3 + 1.5 * interquantile_range
+    low_limit = quartile1 - 1.5 * interquantile_range
+    return low_limit, up_limit
+
+
+def replace_with_thresholds(dataframe, variable):
+    low_limit, up_limit = outlier_thresholds(dataframe, variable)
+    # dataframe.loc[(dataframe[variable] < low_limit), variable] = low_limit
+    dataframe.loc[(dataframe[variable] > up_limit), variable] = up_limit
+
+
+def crm_data_prep(dataframe):
+    dataframe.dropna(axis=0, inplace=True)
+    dataframe = dataframe[~dataframe["Invoice"].str.contains("C", na=False)]
+    dataframe = dataframe[dataframe["Quantity"] > 0]
+    replace_with_thresholds(dataframe, "Quantity")
+    replace_with_thresholds(dataframe, "Price")
+    dataframe["TotalPrice"] = dataframe["Quantity"] * dataframe["Price"]
+    return dataframe
+
+
+check_df(df)
+df_prep = crm_data_prep(df)
+check_df(df_prep)
+
+def create_rfm(dataframe):
+    today_date = dt.datetime(2011, 12, 11)
+    rfm = dataframe.groupby('Customer ID').agg({'InvoiceDate': lambda date: (today_date - date.max()).days,
+                                                'Invoice': lambda num: num.nunique(),
+                                                "TotalPrice": lambda price: price.sum()})
+
+    rfm.columns = ['recency', 'frequency', "monetary"]
+    rfm = rfm[(rfm['monetary'] > 0)]
+
+    # RFM SKORLARININ HESAPLANMASI
+    rfm["recency_score"] = pd.qcut(rfm['recency'], 5, labels=[5, 4, 3, 2, 1])
+    rfm["frequency_score"] = pd.qcut(rfm["frequency"].rank(method="first"), 5, labels=[1, 2, 3, 4, 5])
+
+    # SEGMENTLERIN ISIMLENDIRILMESI
+    rfm['rfm_segment'] = rfm['recency_score'].astype(str) + rfm['frequency_score'].astype(str)
+
+    seg_map = {
+        r'[1-2][1-2]': 'hibernating',
+        r'[1-2][3-4]': 'at_risk',
+        r'[1-2]5': 'cant_loose',
+        r'3[1-2]': 'about_to_sleep',
+        r'33': 'need_attention',
+        r'[3-4][4-5]': 'loyal_customers',
+        r'41': 'promising',
+        r'51': 'new_customers',
+        r'[4-5][2-3]': 'potential_loyalists',
+        r'5[4-5]': 'champions'
+    }
+
+# her bir müşteri için içinde :"Customer Id, recency, frequency, monetary, rfm_segment" değişkenleri olan bir dataframe oluşturuyoruz
+
+    rfm['rfm_segment'] = rfm['rfm_segment'].replace(seg_map, regex=True)
+    rfm = rfm[["recency", "frequency", "monetary", "rfm_segment"]]
+    return rfm
+
+rfm = create_rfm(df_prep)
+
+#### CRM_Final Feature Ekleme
+
+rfm["total_quantity"]=df_prep.groupby("Customer ID").agg({"Quantity":"sum"})
+rfm["total_shopping"]=df_prep.groupby("Customer ID").agg({"Invoice":"count"})
+
+rfm.head()
+
+def create_cltv_c(dataframe):
+    # avg_order_value
+    dataframe['avg_order_value'] = dataframe['monetary'] / dataframe['frequency']
+
+    # purchase_frequency
+    dataframe["purchase_frequency"] = dataframe['frequency'] / dataframe.shape[0]
+
+    # repeat rate & churn rate
+    repeat_rate = dataframe[dataframe.frequency > 1].shape[0] / dataframe.shape[0]
+    churn_rate = 1 - repeat_rate
+
+    # profit_margin
+    dataframe['profit_margin'] = dataframe['monetary'] * 0.05
+
+    # Customer Value
+    dataframe['cv'] = (dataframe['avg_order_value'] * dataframe["purchase_frequency"])
+
+    # Customer Lifetime Value
+    dataframe['cltv'] = (dataframe['cv'] / churn_rate) * dataframe['profit_margin']
+
+# rfm dataframe'inin içerisine her bir müşterinin "calculated cltv" değerini 1-100 arasında standartlaştırarak koyunuz.
+# (değişken ismi: cltv_c)
+    # minmaxscaler
+    scaler = MinMaxScaler(feature_range=(1, 100))
+    scaler.fit(dataframe[["cltv"]])
+    dataframe["cltv_c"] = scaler.transform(dataframe[["cltv"]])
+
+# rfm dataframe'inin içerisine cltv_c_segment adında bir değişken ekliyoruz
+# Bu değişken cltv_c değişkenini 3'e bölmüş olsun. C,B,A şeklinde segment isimlendirmesi ile
+
+    dataframe["cltv_c_segment"] = pd.qcut(dataframe["cltv_c"], 3, labels=["C", "B", "A"])
+
+    dataframe = dataframe[["recency", "frequency", "monetary", "rfm_segment",
+                           "cltv_c", "cltv_c_segment"]]
+
+    return dataframe
+
+check_df(rfm)
+rfm_cltv = create_cltv_c(rfm)
+check_df(rfm_cltv)
+rfm_cltv.head()
+
+def create_cltv_p(dataframe):
+    today_date = dt.datetime(2011, 12, 11)
+    ## recency kullanıcıya özel dinamik.
+    rfm = dataframe.groupby('Customer ID').agg({'InvoiceDate': [lambda date: (date.max()-date.min()).days,
+                                                                lambda date: (today_date - date.min()).days],
+                                                'Invoice': lambda num: num.nunique(),
+                                                'TotalPrice': lambda TotalPrice: TotalPrice.sum()})
+
+    rfm.columns = rfm.columns.droplevel(0)
+
+    ## recency_cltv_p
+    rfm.columns = ['recency_cltv_p', 'T', 'frequency', 'monetary']
+
+    ## basitleştirilmiş monetary_avg
+    rfm["monetary"] = rfm["monetary"] / rfm["frequency"]
+    rfm.rename(columns={"monetary": "monetary_avg"}, inplace=True)
+
+    # BGNBD için WEEKLY RECENCY VE WEEKLY T'nin HESAPLANMASI
+    ## recency_weekly_cltv_p
+    rfm["recency_weekly_cltv_p"] = rfm["recency_cltv_p"] / 7
+    rfm["T_weekly"] = rfm["T"] / 7
+
+    # KONTROL
+    rfm = rfm[rfm["monetary_avg"] > 0]
+
+    ## recency filtre (daha saglıklı cltvp hesabı için)
+    rfm = rfm[(rfm['frequency'] > 1)]
+    rfm["frequency"] = rfm["frequency"].astype(int)
+
+    # BGNBD
+    bgf = BetaGeoFitter(penalizer_coef=0.01)
+    bgf.fit(rfm['frequency'],
+            rfm['recency_weekly_cltv_p'],
+            rfm['T_weekly'])
+
+# rfm dataframe'inin içerisine her bir müşterinin 1 ve 3 ay içinde yapması beklenen satış sayılarını ekliyoruz
+# (değişken ismi: exp_sales_1_month, exp_sales_3_month)
+    # exp_sales_1_month
+    rfm["exp_sales_1_month"] = bgf.predict(4,
+                                           rfm['frequency'],
+                                           rfm['recency_weekly_cltv_p'],
+                                           rfm['T_weekly'])
+    # exp_sales_3_month
+    rfm["exp_sales_3_month"] = bgf.predict(12,
+                                           rfm['frequency'],
+                                           rfm['recency_weekly_cltv_p'],
+                                           rfm['T_weekly'])
+# rfm dataframe'inin içerisine her bir müşterinin expected_average_profit değerini ekliyoruz
+# (değişken ismi: expected_average_profit)
+    # expected_average_profit
+    ggf = GammaGammaFitter(penalizer_coef=0.01)
+    ggf.fit(rfm['frequency'], rfm['monetary_avg'])
+    rfm["expected_average_profit"] = ggf.conditional_expected_average_profit(rfm['frequency'],
+                                                                            rfm['monetary_avg'])
+# rfm dataframe'inin içerisine her bir müşterinin 6 aylık predicted cltv değerini 1-100 arasında standartlaştırıyoruz
+# (değişken ismi: cltv_p)
+    cltv = ggf.customer_lifetime_value(bgf,
+                                       rfm['frequency'],
+                                       rfm['recency_weekly_cltv_p'],
+                                       rfm['T_weekly'],
+                                       rfm['monetary_avg'],
+                                       time=6,
+                                       freq="W",
+                                       discount_rate=0.01)
+    rfm["cltv_p"] = cltv
+    # minmaxscaler
+    scaler = MinMaxScaler(feature_range=(1, 100))
+    scaler.fit(rfm[["cltv_p"]])
+    rfm["cltv_p"] = scaler.transform(rfm[["cltv_p"]])
+
+# rfm dataframe'inin içerisine cltv_p_segment adında bir değişken ekliyoruz
+# Bu değişken cltv_p değişkenini 3'e bölmüş olsun. C,B,A şeklinde segment isimlendirmesi ie
+    # cltv_p_segment
+    rfm["cltv_p_segment"] = pd.qcut(rfm["cltv_p"], 3, labels=["C", "B", "A"])
+
+    ## recency_cltv_p, recency_weekly_cltv_p
+    dfm = rfm[["recency_cltv_p", "T", "monetary_avg", "recency_weekly_cltv_p", "T_weekly",
+               "exp_sales_1_month", "exp_sales_3_month", "expected_average_profit",
+               "cltv_p", "cltv_p_segment"]]
+
+    return dfm
+
+
+rfm_cltv_p = create_cltv_p(df_prep)
+check_df(rfm_cltv_p)
+crm_final = rfm_cltv.merge(rfm_cltv_p, on="Customer ID", how="left")
+check_df(crm_final)
+crm_final.sort_values(by="monetary_avg", ascending=False).head()
+
+# yeni müşterilere değer biçip nasıl odaklanılacağına yönelik yol gösterir.
+crm_final.sort_values(by="cltv_p", ascending=False).head()
